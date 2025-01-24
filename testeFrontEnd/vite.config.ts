2f74361397412a439dcdcf504cf2fd61add9b481
willsf2021 <willsf2021@gmail.com>
2025-01-24 12:14:37 -0300

    fix -fixed cors policy

--- testeFrontEnd/vite.config.ts
+++ testeFrontEnd/vite.config.ts
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://app.econverse.com.br',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-
-      },
-    },
-  },
   base: '/teste-front-end/'
 })
